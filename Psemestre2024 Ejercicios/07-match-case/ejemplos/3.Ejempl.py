537d46eb85f06457654564a17a6abd1158d6c1f6
SGuerra7 <143293169+SGuerra7@users.noreply.github.com>
2024-09-10 01:03:28 -0500

    Add files via upload
    Actualización Ejercicios/Ejemplos

---
+++ Psemestre2024 Ejercicios/07-match-case/ejemplos/3.Ejempl.py
@@ -0,0 +1,11 @@
+color = input("Ingrese Color:")
+
+match color:
+    case "rojo":
+        print("El color es rojo")
+    case "azul":
+        print("El color es azul")
+    case "verde":
+        print("El color es verde")
+    case _:
+        print("El color no es rojo, azul o verde")
