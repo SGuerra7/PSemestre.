06e9e946e7bc4b75996baddf5a793141072e4244
SGuerra7 <143293169+SGuerra7@users.noreply.github.com>
2024-09-10 01:01:45 -0500

    Delete Psemestre2024-2-main directory

--- Psemestre2024-2-main/01-Basic/10.CalculadoraIMC.py
+++
@@ -1,7 +0,0 @@
-Peso = float(input("Digite el peso en Kilogramos:"))
-Altura = float(input("Digite Altura en Metros:"))
-
-IMC = Peso / Altura ** 2
-
-print("El indice de masa corporal es:", IMC)
-
--- Psemestre2024-2-main/02-operLogic/8.MenuOp.py
+++
@@ -1,8 +0,0 @@
-nombre = input("Ingrese Nombre:")
-edad = int(input("Ingrese Edad:"))
-
-if edad >= 18:
-    print(F"{nombre} tienes {edad} años. Por lo tanto puedes votar")
-else:
-    print(f"{nombre} tienes {edad} años. Por lo tanto no puedes votar")
-
