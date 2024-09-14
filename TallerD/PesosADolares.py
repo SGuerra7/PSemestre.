4981b7e10130b89f06f3610140b89eabe058a7a6
SGuerra7 <143293169+SGuerra7@users.noreply.github.com>
2024-09-13 20:15:02 -0500

    Delete TallerD directory

--- TallerD/PesosADolares.py
+++
@@ -1,9 +0,0 @@
-precio_dolar = 4108.63
-dolar = 4108.63
-pesos = float(input("Ingrese cantidad en Pesos: "))
-
-def pesos_a_dolares():
-    convercion = pesos / precio_dolar
-    print(f" El valor en Pesos convertido a Dolares es: {convercion}")
-
-pesos_a_dolares()
